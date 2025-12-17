a080cf7efb37c786e9797a32105673fa87d37a28
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-12-17 12:30:01 +0530

    Add files via upload

---
+++ program_(27).js
@@ -0,0 +1,18 @@
+
+function CountDigit(No)
+{
+    
+}
+
+function main()
+{
+    let iValue = 721;
+
+    console.log(iValue);             //721
+    console.log(typeof(iValue));     //number
+    console.log(iValue % 10);        //1
+    console.log(iValue / 10);        //72
+
+}
+
+main();
