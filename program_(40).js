d0b7f3a9e7d15fce5b2f2c75ec2f51056ba6b8af
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-12-18 09:06:00 +0530

    Add files via upload

---
+++ program_(40).js
@@ -0,0 +1,14 @@
+
+function main()
+{
+    let Arr = [11,90.90,"Hello"];      //Heterogeneous
+
+    console.log(Arr[0]);
+    console.log(Arr[1]);
+    console.log(Arr[2]);
+
+    console.log(typeof(Arr));
+
+}
+
+main();
