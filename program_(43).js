6c6263c35551140302cbfc8de6478852d2bf3e68
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-12-18 09:07:30 +0530

    Add files via upload

---
+++ program_(43).js
@@ -0,0 +1,16 @@
+
+function main()
+{
+    const Arr = [10, 20, 30, 40, 50];    
+
+    let iCnt = 0;
+
+    for(iCnt = 0; iCnt < Arr.length; iCnt++)
+    {
+        console.log(Arr[iCnt]);
+    }
+
+    console.log(typeof(Arr));
+}
+
+main();
